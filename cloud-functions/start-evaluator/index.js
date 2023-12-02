63d0ab240f894fd7c21be3bee701d1d760249779
nemaberci <bercike445@gmail.com>
2023-12-02 14:40:18 +0100

    Solution evaluator

--- cloud-functions/start-evaluator/index.js
+++ cloud-functions/start-evaluator/index.js
@@ -6,13 +6,6 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const {randomUUID} = require('crypto');
 const {sign} = require('jsonwebtoken');
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
-
-// initializeApp({
-//     credential: applicationDefault()
-// });
-initializeApp();
 
 async function getCredentials(cluster, zone) {
     const client = new googleContainer.ClusterManagerClient();
@@ -39,7 +32,7 @@ async function getCredentials(cluster, zone) {
 
 const eventHandler = async cloudEvent => {
 
-    const baseYaml = fs.readFileSync('runner-job.yaml', 'utf8');
+    const baseYaml = fs.readFileSync('evaluator-job.yaml', 'utf8');
     const storage = new Storage();
     let privateKey = await storage.bucket("internal-keys").file("private2.pem").download();
     let privateKeyValue = privateKey[0].toString();
@@ -73,25 +66,6 @@ const eventHandler = async cloudEvent => {
     const podIp = pods[0].status.podIP;
     const uploadFolderName = randomUUID();
 
-    const db = getFirestore();
-
-    const challengeQuery = await db.doc(`Challenge/${cloudEvent.data.message.attributes.challengeId}`).get();
-    const challenge = challengeQuery.data()
-    console.log(challenge)
-    const testcases = await db.doc(`Challenge/${cloudEvent.data.message.attributes.challengeId}`).collection("Testcases").get();
-    const testCaseDocs = testcases.docs.map(d => d.data())
-    console.log(testCaseDocs)
-    let testCasesGenerated = "";
-    let testCaseLocations = "";
-
-    for (let i = 0; i < testCaseDocs.length; i++) {
-        const testCase = testCaseDocs[i];
-        const location = testCase.location;
-        const isGenerated = testCase.is_generated;
-        testCasesGenerated += `${testCasesGenerated.length > 0 ? ";" : ""}${isGenerated ? "true" : "false"}`;
-        testCaseLocations += `${testCaseLocations.length > 0 ? ";" : ""}${location}`;
-    }
-
     let formattedYaml = baseYaml.replace(
         "<FILE_HANDLER_URL>",
         `${podIp}:3000`
@@ -99,47 +73,22 @@ const eventHandler = async cloudEvent => {
         "<TOKEN>",
         sign({}, privateKeyValue, { expiresIn: "1h", algorithm: "RS256" })
     ).replace(
-        "<ENTRY_POINT>",
-        cloudEvent.data.message.attributes.entryPoint
-    ).replace(
         "<CHALLENGE_ID>",
         cloudEvent.data.message.attributes.challengeId
     ).replace(
-        "<EXECUTABLE_FOLDER_NAME>",
-        cloudEvent.data.message.attributes.executableFolderName
-    ).replace(
-        "<INPUT_PY_FOLDER_NAME>",
-        challenge.script_location
-    ).replace(
-        "<INPUT_TXT_FOLDER_NAME>",
-        challenge.text_location
-    ).replace(
-        "<TEST_CASES_GENERATED>",
-        testCasesGenerated
-    ).replace(
-        "<TEST_CASES_LOCATION>",
-        testCaseLocations
+        "<SOLUTION_ID>",
+        cloudEvent.data.message.attributes.solutionId
     ).replace(
         "<RESULTS_FOLDER_NAME>",
-        uploadFolderName
+        cloudEvent.data.message.attributes.targetFolderName
     )
     const spec = yaml.load(formattedYaml);
     console.log(formattedYaml);
 
-    // const batchApi = kc.makeApiClient(k8s.BatchV1Api);
-    // let job = await batchApi.createNamespacedJob('runner', spec);
-    // console.log(job);
+    const batchApi = kc.makeApiClient(k8s.BatchV1Api);
+    let job = await batchApi.createNamespacedJob('runner', spec);
+    console.log(job);
 }
 
 functions.cloudEvent('helloPubSub', eventHandler);
-eventHandler({
-    data: {
-        message: {
-            attributes: {
-                entryPoint: "Main",
-                challengeId: "vFoKe4ncsq6nTxDoGGNb",
-                executableFolderName: "ligma2_built"
-            }
-        }
-    }
-})
+
--- test-challenges/maths/output-verifier/verifier.py
+++ test-challenges/maths/output-verifier/verifier.py
@@ -1,4 +1,6 @@
 
-def verify(input_file_contents, output_file_contents):
-    return int(output_file_contents) == int(input_file_contents[0]) + int(input_file_contents[1])
 
+def verify(input_file_contents, output_file_contents):
+    print(int(output_file_contents[0]), int(input_file_contents[0]) + int(input_file_contents[1]))
+    print(int(output_file_contents[1]), int(input_file_contents[0]) * int(input_file_contents[1]))
+    return int(output_file_contents[0]) == int(input_file_contents[0]) + int(input_file_contents[1]) and int(output_file_contents[1]) == int(input_file_contents[0]) * int(input_file_contents[1])
