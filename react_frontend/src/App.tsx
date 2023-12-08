fdca0a01b0aeb34ef0f9de020b0a3ea993e58f39
SakiiCode <sakiicode10@gmail.com>
2023-12-08 12:31:35 +0100

    Implement firebase auth on frontend

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -5,22 +5,26 @@ import Highscores from "./Highscores/Highscores";
 import Upload from "./NewChallenge/Upload";
 import Frame from "./components/Frame";
 import EditTestCases from "./EditTestCases/EditTestCases";
+import { FirebaseAppProvider } from "reactfire";
+import { firebaseConfig } from "@lib/firebase";
 
 export default function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path="" element={<Frame />}>
-						<Route path="/editor/:id" element={<SolutionEditor />} />
-						<Route path="/highscores/:id" element={<Highscores />} />
-						<Route path="/upload" element={<Upload />} />
-						<Route path="/edit/:id/testcases" element={<EditTestCases />} />
-						<Route path="/" element={<ChallengeList />} />
-					</Route>
-					<Route path="*" element="Oops, page not found" />
-				</Routes>
-			</BrowserRouter>
+			<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+				<BrowserRouter>
+					<Routes>
+						<Route path="" element={<Frame />}>
+							<Route path="/editor/:id" element={<SolutionEditor />} />
+							<Route path="/highscores/:id" element={<Highscores />} />
+							<Route path="/upload" element={<Upload />} />
+							<Route path="/edit/:id/testcases" element={<EditTestCases />} />
+							<Route path="/" element={<ChallengeList />} />
+						</Route>
+						<Route path="*" element="Oops, page not found" />
+					</Routes>
+				</BrowserRouter>
+			</FirebaseAppProvider>
 		</>
 	);
 }
--- react_frontend/src/components/NavBar.tsx
+++ react_frontend/src/components/NavBar.tsx
@@ -1,9 +1,41 @@
-import { DotsThree } from "@phosphor-icons/react";
-import { Button, Navbar } from "react-daisyui";
+import { CircleNotch } from "@phosphor-icons/react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { Button, Navbar, Tooltip } from "react-daisyui";
 import { useNavigate } from "react-router-dom";
+import { useAuth, useSigninCheck, useUser } from "reactfire";
+
+const googleAuthProvider = new GoogleAuthProvider();
 
 export default function NavBar() {
 	const navigate = useNavigate();
+
+	const auth = useAuth();
+
+	const { data: user } = useUser();
+	const { status: signInStatus, data: signInCheckResult } = useSigninCheck();
+
+	let loginButton = <></>;
+
+	if (signInStatus === "loading") {
+		loginButton = <NavBarButtonsLoading />;
+	} else if (signInStatus === "error") {
+		loginButton = <Button color="error">Auth Error</Button>;
+	} else if (signInStatus === "success") {
+		if (signInCheckResult.signedIn) {
+			loginButton = (
+				<NavBarButtonsSignedIn displayName={user?.displayName ?? ""} logout={() => auth.signOut()} />
+			);
+		} else {
+			loginButton = (
+				<NavBarButtonsGuest
+					action={() => {
+						signInWithPopup(auth, googleAuthProvider);
+					}}
+				/>
+			);
+		}
+	}
+
 	return (
 		<>
 			<Navbar className="fixed z-20 bg-base-100">
@@ -18,15 +50,42 @@ export default function NavBar() {
 					</Button>
 				</div>
 				<div className="flex-none gap-2">
-					<Button color="primary" onClick={() => navigate("/upload")}>
-						Új feladat feltöltése
-					</Button>
-					<Button color="primary">BEJELENTKEZÉS</Button>
+					{loginButton}
+					{/*
 					<Button shape="square" color="ghost">
 						<DotsThree size={24} />
-					</Button>
+					</Button>*/}
 				</div>
 			</Navbar>
 		</>
+	);
+}
+
+function NavBarButtonsLoading() {
+	return <CircleNotch className="spinning-slow" size={24} />;
+}
+
+function NavBarButtonsSignedIn({ displayName, logout }: { displayName: string; logout: () => void }) {
+	const navigate = useNavigate();
+
+	return (
+		<>
+			<Button color="primary" onClick={() => navigate("/upload")}>
+				Új feladat feltöltése
+			</Button>
+			<Tooltip message="Kijelentkezés" position="bottom">
+				<Button color="info" onClick={logout}>
+					{displayName}
+				</Button>
+			</Tooltip>
+		</>
+	);
+}
+
+function NavBarButtonsGuest({ action }: { action: () => void }) {
+	return (
+		<Button color="primary" onClick={action}>
+			BEJELENTKEZÉS
+		</Button>
 	);
 }
